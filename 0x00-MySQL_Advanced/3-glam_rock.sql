6efc9b920dcf3f0d6592dc55b6d2ae789554ca9b
youssef nagib <youssefnagib14@yahoo.com>
2024-10-17 14:07:09 +0300

    mysql

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,8 @@
+-- 3-glam_rock.sql
+
+SELECT 
+band_name,
+(IFNULL(split, '2020') - formed) AS lifespan
+    FROM metal_bands
+    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+    ORDER BY lifespan DESC;
