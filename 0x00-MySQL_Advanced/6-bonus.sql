eb4d2693879852ad425f6f3c17075ed502c22e6d
youssef nagib <youssefnagib14@yahoo.com>
2024-10-18 03:11:44 +0300

    mysql

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,28 @@
+-- 6-bonus.sql
+DROP PROCEDURE IF EXISTS AddBonus;
+DELIMITER $$
+CREATE PROCEDURE AddBonus (IN user_id INT,IN project_name VARCHAR(255),in score INT)
+BEGIN
+    DECLARE project_count INT DEFAULT 0;
+    DECLARE project_id INT DEFAULT 0;
+
+    SELECT COUNT(id)
+        INTO project_count
+        FROM projects
+        WHERE name = project_name;
+
+    IF project_count = 0 THEN
+        INSERT INTO projects (name)
+            VALUES (project_name);
+    END IF;
+
+    SELECT id
+        INTO project_id
+        FROM projects
+        WHERE name = project_name;
+
+    INSERT INTO corrections (user_id, project_id, score)
+    VALUES(user_id, project_id, score);
+END$$
+
+DELIMITER ;
