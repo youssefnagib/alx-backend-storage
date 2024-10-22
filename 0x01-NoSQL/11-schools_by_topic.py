506deac17f4b2296da8e1f3f8d71566f44b8c436
youssef nagib <youssefnagib14@yahoo.com>
2024-10-22 23:58:16 +0300

    mongo

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+'''
+schools by topic task 11
+'''
+
+
+def schools_by_topic(mongo_collection, topic):
+    '''
+    Get schools with a given topic
+    :param mongo_collection: MongoCollection object
+    :param topic: topic to search for
+    :return: list of schools that match the topic
+    '''
+    topicFilter = {
+        "topics": {
+            '$elemMatch': {
+                '$eq': topic,
+            },
+        },
+    }
+    return [schools for schools in mongo_collection.find(topicFilter)]
