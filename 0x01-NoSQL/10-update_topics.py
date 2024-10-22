7a576e66f5ee906ba34f53f8dbda55064ceeb477
youssef nagib <youssefnagib14@yahoo.com>
2024-10-22 23:19:05 +0300

    mongo

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -4,7 +4,14 @@ update topics task 10
 '''
 
 
-def update_topics(mongo_collection, name, topics):
+def update_topics(mongo_collection, name, topics) -> None:
+    '''
+    Update the topics for a given school
+    :param mongo_collection: MongoCollection object
+    :param name: name of the school
+    :param topics: list of new topics
+    :return: None
+    '''
     mongo_collection.update_many(
         {"name": name},
         {"$set": {"topics": topics}}
