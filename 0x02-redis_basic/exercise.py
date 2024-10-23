f61cd2418ecab6e61852a0eb63d6115022e6bb70
youssef nagib <youssefnagib14@yahoo.com>
2024-10-24 01:00:20 +0300

    redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 A simple in-memory cache using Redis.
 '''
 import redis
-from typing import Union
+from typing import Union, Optional, Callable
 import uuid
 
 
@@ -27,3 +27,49 @@ class Cache:
         dataKey = str(uuid.uuid4())
         self._redis.set(dataKey, data)
         return dataKey
+
+    def get(self,
+            key: str,
+            fn: Optional[Callable] = None,
+            ) -> Union[str, bytes, int, float, None]:
+        '''
+        Retrieves the data associated with the given key from Redis.
+        Args:
+            key (str): The unique key for the data to retrieve.
+            fn (Optional[Callable], optional): A function to apply
+            to the retrieved data. Defaults to None.
+        Returns:
+            Union[str, bytes, int, float, None]: The retrieved data
+            or None if the key does not exist.
+        '''
+        data = self._redis.get(key)
+
+        if data is None:
+            return None
+
+        if fn:
+            callable_fn = fn(data)
+            return callable_fn
+        else:
+            return data
+
+    def get_str(self, key: str) -> str:
+        '''
+        Retrieves the string representation of the data
+        associated with the given key from Redis.
+        Args:
+            key (str): The unique key for the data to retrieve.
+        Returns:
+            str: The string representation of the retrieved data.
+        '''
+        value = self._redis.get(key, fn=lambda d: d.decode('utf-8'))
+        return value
+
+    def get_int(self, key: str) -> int:
+        ''''''
+        value = self._redis.get(key)
+        try:
+            value = int(value.decode('utf-8'))
+        except Exception:
+            return None
+        return value
--- 0x02-redis_basic/main.py
+++ 0x02-redis_basic/main.py
@@ -5,12 +5,24 @@ Main file
 import redis
 
 Cache = __import__('exercise').Cache
+'''Test 0'''
+# cache = Cache()
+
+# data = b"hello"
+# key = cache.store(data)
+# print(key)
+
+# local_redis = redis.Redis()
+# print(local_redis.get(key))
 
 cache = Cache()
 
-data = b"hello"
-key = cache.store(data)
-print(key)
+TEST_CASES = {
+    b"foo": None,
+    123: int,
+    "bar": lambda d: d.decode("utf-8")
+}
 
-local_redis = redis.Redis()
-print(local_redis.get(key))
+for value, fn in TEST_CASES.items():
+    key = cache.store(value)
+    assert cache.get(key, fn=fn) == value
