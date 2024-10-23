92d86108f6ef92f50167e6f2ac96056d8d585bd2
youssef nagib <youssefnagib14@yahoo.com>
2024-10-24 00:30:16 +0300

    redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -14,7 +14,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[bytes, int, float]) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Stores the given data in Redis and returns a unique key for it.
 
